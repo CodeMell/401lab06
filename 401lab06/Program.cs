a9d87c23dafc5cb449cc07989832d7363fc57a69
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-07-19 06:29:55 -0500

    Update Program.cs

--- 401lab06/Program.cs
+++ 401lab06/Program.cs
@@ -1,109 +1,223 @@
 ﻿using System;
 
+//lab06
+
+//public abstract class Animal
+//{
+//    public string Name { get; set; }
+//    public abstract void Eat();
+//    public abstract void Sleep();
+//}
+
+//public abstract class Mammal : Animal
+//{
+//    public abstract void GiveBirth();
+//}
+
+//public abstract class Reptile : Animal
+//{
+//    public abstract void LayEggs();
+//}
+
+//public class Dog : Mammal
+//{
+//    public override void Eat()
+//    {
+//        Console.WriteLine("The dog is eating.");
+//    }
+
+//    public override void Sleep()
+//    {
+//        Console.WriteLine("The dog is sleeping.");
+//    }
+
+//    public override void GiveBirth()
+//    {
+//        Console.WriteLine("The dog gave birth.");
+//    }
+//}
+
+//public class Cat : Mammal
+//{
+//    public override void Eat()
+//    {
+//        Console.WriteLine("The cat is eating.");
+//    }
+
+//    public override void Sleep()
+//    {
+//        Console.WriteLine("The cat is sleeping.");
+//    }
+
+//    public override void GiveBirth()
+//    {
+//        Console.WriteLine("The cat gave birth.");
+//    }
+//}
+
+//public class Snake : Reptile
+//{
+//    public override void Eat()
+//    {
+//        Console.WriteLine("The snake is eating.");
+//    }
+
+//    public override void Sleep()
+//    {
+//        Console.WriteLine("The snake is sleeping.");
+//    }
+
+//    public override void LayEggs()
+//    {
+//        Console.WriteLine("The snake laid eggs.");
+//    }
+//}
+
+//public class Lizard : Reptile
+//{
+//    public override void Eat()
+//    {
+//        Console.WriteLine("The lizard is eating.");
+//    }
+
+//    public override void Sleep()
+//    {
+//        Console.WriteLine("The lizard is sleeping.");
+//    }
+
+//    public override void LayEggs()
+//    {
+//        Console.WriteLine("The lizard laid eggs.");
+//    }
+//}
+
+//public class Bird : Animal
+//{
+//    public override void Eat()
+//    {
+//        Console.WriteLine("The bird is eating.");
+//    }
+
+//    public override void Sleep()
+//    {
+//        Console.WriteLine("The bird is sleeping.");
+//    }
+
+//    public void Fly()
+//    {
+//        Console.WriteLine("The bird is flying.");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Animal dog = new Dog();
+//        dog.Name = "Dog";
+//        dog.Eat();
+//        dog.Sleep();
+//        ((Mammal)dog).GiveBirth();
+
+//        Console.WriteLine();
+
+//        Animal cat = new Cat();
+//        cat.Name = "Cat";
+//        cat.Eat();
+//        cat.Sleep();
+//        ((Mammal)cat).GiveBirth();
+
+//        Console.WriteLine();
+
+//        Animal snake = new Snake();
+//        snake.Name = "Snake";
+//        snake.Eat();
+//        snake.Sleep();
+//        ((Reptile)snake).LayEggs();
+
+//        Console.WriteLine();
+
+//        Animal lizard = new Lizard();
+//        lizard.Name = "Lizard";
+//        lizard.Eat();
+//        lizard.Sleep();
+//        ((Reptile)lizard).LayEggs();
+
+//        Console.WriteLine();
+
+//        Animal bird = new Bird();
+//        bird.Name = "Bird";
+//        bird.Eat();
+//        bird.Sleep();
+//        ((Bird)bird).Fly();
+//    }
+//}
+
+
+//lab07
+
+public interface IEatable
+{
+    void Eat();
+}
+
+public interface ISleepable
+{
+    void Sleep();
+}
+
 public abstract class Animal
 {
     public string Name { get; set; }
-    public abstract void Eat();
-    public abstract void Sleep();
 }
 
-public abstract class Mammal : Animal
+public class Dog : Animal, IEatable, ISleepable
 {
-    public abstract void GiveBirth();
-}
-
-public abstract class Reptile : Animal
-{
-    public abstract void LayEggs();
-}
-
-public class Dog : Mammal
-{
-    public override void Eat()
+    public void Eat()
     {
         Console.WriteLine("The dog is eating.");
     }
 
-    public override void Sleep()
+    public void Sleep()
     {
         Console.WriteLine("The dog is sleeping.");
     }
-
-    public override void GiveBirth()
-    {
-        Console.WriteLine("The dog gave birth.");
-    }
 }
 
-public class Cat : Mammal
+public class Cat : Animal, IEatable, ISleepable
 {
-    public override void Eat()
+    public void Eat()
     {
         Console.WriteLine("The cat is eating.");
     }
 
-    public override void Sleep()
+    public void Sleep()
     {
         Console.WriteLine("The cat is sleeping.");
     }
-
-    public override void GiveBirth()
-    {
-        Console.WriteLine("The cat gave birth.");
-    }
 }
 
-public class Snake : Reptile
+public class Snake : Animal, IEatable
 {
-    public override void Eat()
+    public void Eat()
     {
         Console.WriteLine("The snake is eating.");
     }
-
-    public override void Sleep()
-    {
-        Console.WriteLine("The snake is sleeping.");
-    }
-
-    public override void LayEggs()
-    {
-        Console.WriteLine("The snake laid eggs.");
-    }
 }
 
-public class Lizard : Reptile
+public class Lizard : Animal, IEatable
 {
-    public override void Eat()
+    public void Eat()
     {
         Console.WriteLine("The lizard is eating.");
     }
-
-    public override void Sleep()
-    {
-        Console.WriteLine("The lizard is sleeping.");
-    }
-
-    public override void LayEggs()
-    {
-        Console.WriteLine("The lizard laid eggs.");
-    }
 }
 
-public class Bird : Animal
+public class Bird : Animal, ISleepable
 {
-    public override void Eat()
-    {
-        Console.WriteLine("The bird is eating.");
-    }
-
-    public override void Sleep()
+    public void Sleep()
     {
         Console.WriteLine("The bird is sleeping.");
-    }
-
-    public void Fly()
-    {
-        Console.WriteLine("The bird is flying.");
     }
 }
 
@@ -113,40 +227,32 @@ class Program
     {
         Animal dog = new Dog();
         dog.Name = "Dog";
-        dog.Eat();
-        dog.Sleep();
-        ((Mammal)dog).GiveBirth();
+        ((IEatable)dog).Eat();
+        ((ISleepable)dog).Sleep();
 
         Console.WriteLine();
 
         Animal cat = new Cat();
         cat.Name = "Cat";
-        cat.Eat();
-        cat.Sleep();
-        ((Mammal)cat).GiveBirth();
+        ((IEatable)cat).Eat();
+        ((ISleepable)cat).Sleep();
 
         Console.WriteLine();
 
         Animal snake = new Snake();
         snake.Name = "Snake";
-        snake.Eat();
-        snake.Sleep();
-        ((Reptile)snake).LayEggs();
+        ((IEatable)snake).Eat();
 
         Console.WriteLine();
 
         Animal lizard = new Lizard();
         lizard.Name = "Lizard";
-        lizard.Eat();
-        lizard.Sleep();
-        ((Reptile)lizard).LayEggs();
+        ((IEatable)lizard).Eat();
 
         Console.WriteLine();
 
         Animal bird = new Bird();
         bird.Name = "Bird";
-        bird.Eat();
-        bird.Sleep();
-        ((Bird)bird).Fly();
+        ((ISleepable)bird).Sleep();
     }
 }
